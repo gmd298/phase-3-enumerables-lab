8d03e26b31d4ae51356218731394aa74e50ffaad
Griffin <gmd298@gmail.com>
2022-04-29 12:10:09 -0400

    commit tree 2a84751e7e7140cd8d6b3fdebfa0555a592dd1fa

--- enumerables.rb
+++ enumerables.rb
@@ -12,22 +12,26 @@ end
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
+  spicy_foods.map do |spicy|
+    "#{spicy[:name]}"
+  end
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
-  # your code here
+  spicy_foods.filter {|spicy_filter| spicy_filter[:heat_level] > 5}
 end
 
 # given an array of spicy foods, **output to the terminal**
 # each spicy food in the following format: 
 # Buffalo Wings (American) | Heat Level: 🌶🌶🌶
-# HINT: you can use * with a string to produce the correct number of 🌶 emoji. 
+# HINT: you can use * with a string to produce the correct number of  emoji. 
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
-  # your code here
+  new_format = spicy_foods.each do |food|
+    p "#{food[:name]} (#{food[:cuisine]}) | #{food[:heat_level]  * "🌶"}"
+  end
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
